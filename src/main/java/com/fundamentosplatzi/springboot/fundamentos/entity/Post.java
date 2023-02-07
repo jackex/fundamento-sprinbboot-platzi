07760362c3f7af0f7ec8ce3c2cf98b63b16eae12
julian certuche <julian.certuche.c@gmail.com>
2023-02-06 22:47:22 -0500

    commit para creacion de las Entity

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/entity/Post.java
@@ -0,0 +1,50 @@
+package com.fundamentosplatzi.springboot.fundamentos.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_post", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", user=" + user +
+                '}';
+    }
+
+    public Post(String description, User user) {
+        this.description = description;
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @ManyToOne
+    private  User user;
+
+}
